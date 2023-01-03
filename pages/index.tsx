7890c0b28ccc65bf2338a2d78ee8c0167ad66f97
confessore <confessore@balasolu.com>
2023-01-03 18:02:43 -0500

    strong and height

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className="flex flex-col justify-center items-center p-3 bg-stone-900 h-screen">
         <Image
           src="/balasolu.svg"
           alt="balasolu"
@@ -22,7 +22,7 @@ export default function Home() {
           width={256}
           priority
           className="invert" />
-          <p className={`${inter.className} text-slate-400`}>we&apos;ll handle the nerd stuff</p>
+          <strong className={`${inter.className} text-slate-400`}>we&apos;ll handle the nerd stuff</strong>
           <a href="sms:+1(352)234-3458" className={`${inter.className} text-amber-400`}><strong>+1 (352) 234 3458</strong></a>
       </div>
     </>
