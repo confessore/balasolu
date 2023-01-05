16570cb9e632efa6934fe6306e9ba1d6b9d19748
confessore <confessore@balasolu.com>
2023-01-05 14:06:29 -0500

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col justify-center items-center p-3 bg-stone-900 h-screen">
+      <div className="flex flex-col justify-center items-center p-3 bg-slate-800 h-screen">
         <Image
           src="/balasolu.svg"
           alt="balasolu"
@@ -24,12 +24,12 @@ export default function Home() {
           className="invert" />
           <strong className={`${inter.className} text-slate-400`}>we&apos;ll handle the nerd stuff</strong>
           <div className="pt-3 text-center">
-            <p className={`${inter.className}`}>Central Florida</p>
-            <a href="sms:+1(352)234-3458" className={`${inter.className} text-amber-400`}><strong>+1 (352) 234 3458</strong></a>
+            <p className={`${inter.className} text-sm`}>North Central Florida</p>
+            <a href="sms:+1(352)234-3458" className={`${inter.className} text-emerald-500 text-lg`}><strong>(352) 234 3458</strong></a>
           </div>
           <div className="pt-3 text-center">
-            <p className={`${inter.className}`}>Northeast Florida</p>
-            <a href="sms:+1(904)853-0345" className={`${inter.className} text-amber-400`}><strong>+1 (904) 853 0345</strong></a>
+            <p className={`${inter.className} text-sm`}>Northeast Florida</p>
+            <a href="sms:+1(904)853-0345" className={`${inter.className} text-emerald-500 text-lg`}><strong>(904) 853 0345</strong></a>
           </div>
       </div>
     </>
