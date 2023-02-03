a771f6195de42bc2d4a8a8ac195dd9e35a28c3b6
steven confessore <confessore@balasolu.com>
2023-02-03 11:32:39 -0500

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,16 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import Layout from '../components/layout';
 import Tawk from '../components/tawk';
+import GTag from '../components/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GTag />
       <Layout>
         <Component {...pageProps} />
       </Layout>
       <Tawk />
-      <Analytics />
     </>
   )
 }
