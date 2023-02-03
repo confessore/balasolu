a87352b2f0e26ff73da6189c2443554cdb52f21e
steven confessore <confessore@balasolu.com>
2023-02-03 11:40:46 -0500

    ga fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react';
-import Layout from '../components/layout';
-import Tawk from '../components/tawk';
-import GTag from '../components/gtag';
+import { Analytics } from '@vercel/analytics/react'
+import Layout from '../components/layout'
+import Tawk from '../components/tawk'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GTag />
+      <GoogleAnalytics trackPageViews />
       <Layout>
         <Component {...pageProps} />
       </Layout>
