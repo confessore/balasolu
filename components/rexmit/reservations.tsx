e7bfecb080e44a23d56f6a0b1b320f764837392a
confessore <confessore@balasolu.com>
2023-05-18 13:34:46 -0400

    prune

--- components/rexmit/reservations.tsx
+++ components/rexmit/reservations.tsx
@@ -17,18 +17,17 @@ type Props = {
 }
 
 const reservation_options = [
-    ["rexmit +1 Month", 9],
-    ["rexmit +3 Months", 27],
-    ["rexmit +6 Months", 54],
-    ["rexmit +12 Months", 108]
+    ["+1 Month", 10],
+    ["+3 Months", 27],
+    ["+6 Months", 48],
+    ["+12 Months", 84]
 ]
 
-const btw = "flex flex-wrap justify-center items-center py-5"
-const options = "flex flex-col justify-center items-center py-5 gap-y-3"
-const addons = "flex flex-col justify-center items-center p-5"
-const pricing = "grid grid-cols-2 justify-center items-center py-5"
-const p = "w-full rounded-xl text-lg text-center justify-center items-center text-gray-800 dark:text-gray-200"
-const h2 = "font-[600] mb-3 opacity-60 max-w-full rounded-xl p-5 text-2xl text-center justify-center items-center text-gray-800 dark:text-gray-200"
+const btw = "flex flex-wrap justify-center items-center py-1"
+const options = "flex flex-col justify-center items-center py-1 gap-y-1"
+const pricing = "grid grid-cols-2 justify-center items-center py-1"
+const p = "rounded-xl text-lg text-center justify-center items-center text-gray-800 dark:text-gray-200 p-1 w-[256px]"
+const h2 = "font-[600] mb-3 opacity-60 max-w-full rounded-xl p-1 text-2xl text-center justify-center items-center text-gray-800 dark:text-gray-200"
 const selected = "bg-blue-500"
 const input = "bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border h-12 border-gray-800 dark:border-gray-200 rounded-md p-3"
 
@@ -44,13 +43,10 @@ type PurchaseUnit = {
 
 const Reservations = (props: Props) => {
     const [selectedIndex, setSelectedIndex] = useState(0)
-    const [pickupRequired, setPickupRequired] = useState(false)
     const [items, setItems] = useState({} as any[])
     const [subtotal, setSubtotal] = useState(0)
     const [surcharge, setSurcharge] = useState(0)
     const [total, setTotal] = useState(0)
-    const [description, setDescription] = useState("")
-    const [quantity, setQuantity] = useState(0)
 
     const createItem = (
         name: string,
@@ -72,12 +68,6 @@ const Reservations = (props: Props) => {
     const getSelectedPackagePrice = () => 
         (reservation_options[selectedIndex][1] as number)
 
-    const getAddonName = (index: number) =>
-        (reservation_options[index][0] as string)
-
-    const getAddonPrice = (index: number) =>
-        (reservation_options[index][1] as number)
-
     const createAmount = (value: number) => {
         return {currency_code: 'USD', value: value}
     }
@@ -92,16 +82,6 @@ const Reservations = (props: Props) => {
             ),
         ]
 
-        if (pickupRequired) { 
-            items.push(
-                createItem(
-                    getAddonName(0),
-                    1,
-                    createAmount(getAddonPrice(0)),
-                    getAddonName(0)
-            ))
-        }
-
         items.push(
             createItem("Surcharge",
             1,
@@ -115,7 +95,7 @@ const Reservations = (props: Props) => {
     }
 
     const calcSubtotal = () => {
-        const subtotal = getSelectedPackagePrice() + (pickupRequired ? getAddonPrice(0) : 0)
+        const subtotal = getSelectedPackagePrice();
         setSubtotal(subtotal)
         return subtotal
     }
@@ -147,7 +127,7 @@ const Reservations = (props: Props) => {
                         return (
                             <p 
                                 key={index}
-                                className={`${p} ${index === selectedIndex ? selected : ""} border border-gray-800 dark:border-gray-200 p-5`}
+                                className={`${p} ${index === selectedIndex ? selected : ""} border border-gray-800 dark:border-gray-200 p-1`}
                                 onClick={() => { setSelectedIndex(index) }}>
                                     {kvp[0]}
                             </p>
