f3717f269c6f9c2ae09ff8fb5196cb707606f0b5
confessore <confessore@balasolu.com>
2023-08-08 21:17:44 -0400

    p-8

--- components/overlay.tsx
+++ components/overlay.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Overlay() {
   return (
-    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex p-8">
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Feel free to call or text&nbsp;
         <code className="font-mono font-bold">
