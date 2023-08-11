dee612ec1156ab8f86369f02dddcf4579206492a
confessore <confessore@balasolu.com>
2023-08-11 16:43:32 -0400

    apos

--- components/conclusion.tsx
+++ components/conclusion.tsx
@@ -5,8 +5,8 @@ export default function Conclusion() {
     <p className="font-light text-center p-8">
       At Balanced Solutions Software, we are your partners in innovation. From
       development to hosting, we provide end-to-end solutions that empower your
-      digital presence. Let's embark on a journey to create applications that
-      drive success. Contact us today to explore how we can transform your
+      digital presence. Let&apos;s embark on a journey to create applications
+      that drive success. Contact us today to explore how we can transform your
       digital aspirations into reality.
     </p>
   );
