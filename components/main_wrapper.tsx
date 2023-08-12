773455b043912ea7a1e7eda17f08eaca7b48d924
confessore <confessore@balasolu.com>
2023-08-11 20:09:58 -0400

    quotes

--- components/main_wrapper.tsx
+++ components/main_wrapper.tsx
@@ -4,7 +4,7 @@ export default function MainWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen min-w-full flex-col items-center justify-between gap-3 bg-[url(/jwt.webp)] py-10 text-pink-900 opacity-90 dark:text-pink-200">
+    <main className="flex min-h-screen min-w-full flex-col items-center justify-between gap-3 bg-[url('/jwt.webp')] py-10 text-pink-900 opacity-90 dark:text-pink-200">
       {children}
     </main>
   );
