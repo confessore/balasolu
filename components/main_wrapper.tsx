de0b1d84e27def36403dc32daf8667c2d346c13d
confessore <confessore@balasolu.com>
2023-08-11 20:53:43 -0400

    optimization

--- components/main_wrapper.tsx
+++ components/main_wrapper.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+
 export default function MainWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen min-w-full flex-col items-center justify-between gap-3 bg-[url('/jwt.webp')] py-10 text-pink-900 opacity-90 dark:text-pink-200">
+    <main className="flex min-h-screen min-w-full flex-col items-center justify-between gap-3 py-10 text-pink-900 opacity-90 dark:text-pink-200 lg:py-0 lg:pb-10">
+      <div className="fixed h-[100vh] w-[100vw]">
+        <Image
+          src={"/jwt.webp"}
+          layout="fill"
+          objectFit="cover"
+          priority
+          alt={"jwt"}
+        />
+      </div>
       {children}
     </main>
   );
