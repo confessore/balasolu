837319429f496370e26b43c9587c9de0085df691
confessore <confessore@balasolu.com>
2023-08-11 23:00:03 -0400

    webp

--- components/main_wrapper.tsx
+++ components/main_wrapper.tsx
@@ -9,7 +9,7 @@ export default function MainWrapper({
     <main className="flex min-h-screen min-w-full flex-col items-center justify-between gap-3 py-10 text-pink-900 opacity-90 dark:text-pink-200 lg:py-0 lg:pb-10">
       <div className="fixed inset-0 -z-10 h-[100vh] w-[100vw]">
         <Image
-          src={"/jwt.avif"}
+          src={"/jwt.webp"}
           alt={"jwt"}
           fill
           style={{objectFit: "cover"}}
