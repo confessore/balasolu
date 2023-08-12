067054bd0768c1f07ee52640aa7b9471a56133b4
confessore <confessore@balasolu.com>
2023-08-11 20:16:23 -0400

    nav link opacity

--- components/nav_link.tsx
+++ components/nav_link.tsx
@@ -16,15 +16,13 @@ export default function NavLink(props: Props) {
       target={props.blank ? "_blank" : ""}
       rel="noopener noreferrer"
     >
-      <h2 className={`mb-3 text-2xl font-semibold`}>
+      <h2 className="mb-3 text-2xl font-semibold">
         {props.title}{" "}
         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
           -&gt;
         </span>
       </h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-        {props.description}
-      </p>
+      <p className="m-0 max-w-[30ch] text-sm opacity-60">{props.description}</p>
     </Link>
   );
 }
