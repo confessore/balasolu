6e267d3a2fa89e0d3ad151fdd738b4a306690a48
confessore <confessore@balasolu.com>
2024-04-12 18:17:45 -0400

    pages

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+import FlexColumn from "@/components/flex_column";
+import MetaInfo from "@/components/meta_info";
+import type { Metadata } from "next";
+
+const title = "About";
+const description = "";
+
+export const metadata: Metadata = {
+  title: `${title} - Balanced Solutions Software - Seamless Development and Hosting Solutions for Web, Mobile, and Desktop Applications`,
+  description: description,
+};
+
+export default function Page() {
+  return (
+    <FlexColumn>
+      <MetaInfo title={title} description={description} />
+    </FlexColumn>
+  );
+}
