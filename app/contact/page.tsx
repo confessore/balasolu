8bf5b60aa90bf003393dc0f592f922d54df8863b
confessore <confessore@balasolu.com>
2024-04-12 19:05:54 -0400

    contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import MetaInfo from "@/components/meta_info";
 import type { Metadata } from "next";
 
 const title = "Contact";
-const description = "";
+const description = "You can reach us by text or phone at (352) 234 3458";
 
 export const metadata: Metadata = {
   title: `${title} - Balanced Solutions Software - Seamless Development and Hosting Solutions for Web, Mobile, and Desktop Applications`,
