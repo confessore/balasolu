b3fdcdb304a37a0f987e5b2a5bd46472c6ef1e67
confessore <confessore@balasolu.com>
2026-03-22 12:41:35 -0400

    fix: svelte

--- components/flex_wrap.tsx
+++
@@ -1,7 +0,0 @@
-export default function FlexWrap({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-wrap items-center justify-center gap-3">
-      {children}
-    </div>
-  );
-}
